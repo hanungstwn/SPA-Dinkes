5004540646d2525f0ffaf7f68c4be45284139aab
hanung setiawan <hanungsetiawan23@gmail.com>
2023-03-08 23:24:19 +0700

    fix-accordion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
-const dropdownList = [...dropdownElementList].map(
-  (dropdownToggleEl) => new bootstrap.Dropdown(dropdownToggleEl)
-);
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+
+var panel = this.nextElementSibling;
+if (panel.style.maxHeight){
+  panel.style.maxHeight = null;
+} else {
+  let active = document.querySelectorAll(".accordion-div .accordion.active");
+  for(let j = 0; j < active.length; j++){
+    active[j].classList.remove("active");
+    active[j].nextElementSibling.style.maxHeight = null;
+  }
+  this.classList.toggle("active");
+  panel.style.maxHeight = panel.scrollHeight + "px";
+}
+});
+}
